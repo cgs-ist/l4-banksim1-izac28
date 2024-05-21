00ea14855e2067472cd71c7e241c9a1ad5445fac
fizzixww <00fizzixww00@gmail.com>
2024-05-21 15:13:10 +1000

    fixed code

--- main.py
+++ main.py
@@ -5,22 +5,54 @@
 accountName = 'Joe'
 accountBalance = 100
 accountPassword = 'soup'
-
-while True:
+i = 3
+def balance():
+    return str(accountBalance)
+def deposit(amount):
+    global accountBalance
+    accountBalance += amount
+    return("deposited " + str(amount) + " into your account.")
+def withdrawl(amount):
+    global accountBalance
+    accountBalance -= amount
+    return("withdrew " + str(amount) + " from your account.")
+def show():
+    global accountName 
+    global accountBalance 
+    global accountPassword
+    print(f"Account name: {accountName}\nAccount Balance: {accountBalance}\nAccount password: {accountPassword}")
+def instructions():
     print('Press b to get the balance')
     print('Press d to make a deposit')
     print('Press w to make a withdrawl')
     print('Press s to show the account')    
     print('Press q to quit')
-    
-    action = input("What do you want to do? ")
-    action = action.lower() #
-    action = action[0]
-    
-    if action == 'b':
-        print('Get Balance:')
-        userPassword = input('Please enter your password: ')
-        if userPassword != accountPassword:
-            print('Incorrect Password')
+while i > 0:
+    userPassword = input('Please enter your password: ')
+    jj = True
+    while userPassword == accountPassword:
+        if jj == True:
+            instructions()
+        jj = False
+        action = input("What do you want to do? ")
+        action = action.lower()
+        action = action[0]
+        if action == 'b':
+            print(balance())
+        elif action == 'd':
+            print(deposit(int(input("How much would you like to deposit?"))))
+        elif action == 'w':
+            print(withdrawl(int(input("How much would you like to withdrawl?"))))
+        elif action == 's':
+            show()
+        elif action == 'q':
+            exit()
         else:
-            print('Your balance is: ', accountBalance)
+            print("Enter a valid action")
+            instructions()
+    i = i-1
+    print(f"Incorrect password. {i} attempts left.")
+print("Too many failed attempts. Terminating your family.")
+exit()
+
+        
